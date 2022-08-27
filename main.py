a78b17c5851904863dd124524b2a726b41b8bcaa
Dilyarin <dilyarin@gmail.com>
2022-08-27 14:10:44 +0300

    delete duplicates added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 with open("phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-pprint(contacts_list)
+  # pprint(contacts_list)
 
 pattern = r"(\+7|8)[\s(]*(\d{3})[)\s-]*(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})?[\s]?[(]?(\доб.)?[\s-]?[\s-]?(\d{4})?[)\s-]?"
 new_pattern = r"+7(\2)\3-\4-\5 \6\7"
@@ -28,13 +28,42 @@ for rec in new_contact_list:
   page_list = format_page.split(',')
   if page_list not in contacts_list:
     contacts_list.append(page_list)
-    print(contacts_list)
+# pprint(contacts_list)
+
+new_list = [contacts_list [0]]
+contacts_list.pop(0)
+contacts_list.sort()
+new_list.append(contacts_list[0])
+i = 1
+for rec in contacts_list:
+  if rec[0] == new_list[i][0] and rec[1] == new_list[i][1]:
+    if rec[2] != '':
+      new_list[i][2] = rec[2]
+    if rec[3] != '':
+      new_list[i][3] = rec[3]
+    if rec[4] != '':
+      new_list[i][4] = rec[4]
+    if rec[5] != '':
+      new_list[i][5] = rec[5]
+    if rec[6] != '':
+      new_list[i][6] = rec[6]
+  else:
+    new_list.append(rec)
+    i = i+1
+
+  # temp_voc = {}
+  # voc={}
+  # for record in list:
+  #   temp_voc[record[0], record[1]] = (record[2], record[3], record[4], record[5], record[6])
+  #
+  # for fio, data in temp_voc.items():
+  #   if data [0] == 0:
+  #     voc[fio] = data
+  # pprint(voc)
+
 
 with open("phonebook.csv", "w", encoding='utf-8') as f:
   datawriter = csv.writer(f, delimiter=',')
-  datawriter.writerows(contacts_list)
-
-
-
+  datawriter.writerows(new_list)
 
 
